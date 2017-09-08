201be1329782f154d5c686eb4a2cd6f95818bad8
drnobodyphd <rchase@ltu.edu>
2017-09-08 17:05:17 -0400

    initial commit

---
+++ module.c
@@ -0,0 +1,118 @@
+//**Quick Instructions**//
+
+//This is the c file for Quiz3.
+//Insert your code and documentation as described in the
+//software design lecture.  
+//Only insert code in between your group number and before
+//the next group number for each section.  
+//Do not delete any text.
+
+
+//**Begin File**//
+
+//descriptions************
+
+//group1
+
+//group2
+
+//group3
+
+//group4
+
+//group5
+
+//group6
+
+
+
+//external variables *********
+
+//group1
+
+//group2
+
+//group3
+
+//group4
+
+//group5
+
+//group6
+
+
+//includes************
+
+#include "module.h"
+
+//defines************
+													//ring has 60 leds*
+//group1
+
+//group2
+
+//group3
+
+//group4
+
+//group5
+
+//group6
+
+//structs************
+
+//group1
+
+//group2
+
+//group3
+
+//group4
+
+//group5
+
+//group6
+
+
+//global variables ************
+
+
+//group1
+
+//group2
+
+//group3
+
+//group4
+
+//group5
+
+//group6
+
+//private prototypes ************
+
+//group1
+
+//group2
+
+//group3
+
+//group4
+
+//group5
+
+//group6
+
+//code ************
+
+//group1
+
+//group2
+
+//group3
+
+//group4
+
+//group5
+
+//group6
