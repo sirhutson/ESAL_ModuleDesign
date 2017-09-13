33d0aa016e586b69357ac1cfd008673ffadc814e
sirhutson <31014881+sirhutson@users.noreply.github.com>
2017-09-13 13:39:23 -0400

    nops added

--- module.c
+++ module.c
@@ -112,11 +112,23 @@
 //group3
 void send_one()  //user inputs the letter(port) and number(pin)
 {
+	int i;
+	
 	//might need a for loop to reach time requirement
 	GPIOPinWrite( WSPort, WSPin, 1);
-	//some delay
+	for (i=0; i<9;i++)
+	{
+		__nop;
+	}
+	//using nop could just delay system for around the right time with the possible error
 	GPIOPinWrite(WSPort, WSPin, 0);
+		for (i=0; i<7;i++)
+	{
+		__nop;
+	}
 	
+	//with a clock speed of 16MHz it is 62.5ns to complete an instruction
+	//each instruction is 1 clock cycle
 }
 //group4
 
